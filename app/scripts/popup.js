da91df83d962f437824ccacd8b8aa70e24d3a534
John Ghidiu <jghidiu@solutechnology.com>
2015-03-31 21:31:06 -0400

    -Added travis-ci integration -New images -Formatting fixes

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -4,7 +4,7 @@
     'use strict';
 
     $(document).ready(function() {
-         $('#image-placeholder').append('<a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif"></a>');
+        $('#image-placeholder').append('<a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif"></a>');
     });
 
 })();
