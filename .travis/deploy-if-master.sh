97c38beb2726843e87d2be0eca40dc5a55670057
John Ghidiu <jghidiu@solutechnology.com>
2018-04-18 17:53:52 -0400

    Working on Travis config

--- .travis/deploy-if-master.sh
+++ .travis/deploy-if-master.sh
@@ -3,8 +3,6 @@
 ## Publishes the extension to the Chrome Web Store if on the master branch
 if [[ 'master' == $TRAVIS_BRANCH ]]; then
   echo "Publishing on branch $TRAVIS_BRANCH"
-  grunt release
-  ls package/
   grunt publish --clientId=$CLIENT_ID --clientSecret=$CLIENT_SECRET --refreshToken=$REFRESH_TOKEN
 else
   echo "Skipping deploy on branch $TRAVIS_BRANCH"
