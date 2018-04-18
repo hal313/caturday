6510003a24fa68d45917d3b2e548bcb2b8ad9536
John Ghidiu <jghidiu@solutechnology.com>
2018-04-18 19:34:49 -0400

    Repointed publish branch in deploy script

--- .travis/deploy-if-master.sh
+++ .travis/deploy-if-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Publishes the extension to the Chrome Web Store if on the master branch
-if [[ 'citest' == $TRAVIS_BRANCH ]]; then
+if [[ 'master' == $TRAVIS_BRANCH ]]; then
   echo "Publishing on branch $TRAVIS_BRANCH"
   grunt publish --clientId=$CLIENT_ID --clientSecret=$CLIENT_SECRET --refreshToken=$REFRESH_TOKEN
 else
