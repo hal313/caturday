d457f9f5a9dfce8c0447371a214f235e4f275d67
John Ghidiu <jghidiu@solutechnology.com>
2018-04-18 16:42:54 -0400

    Fixed Travis config to call correct script

--- .travis/deploy-if-master.sh
+++ .travis/deploy-if-master.sh
@@ -3,4 +3,6 @@
 ## Publishes the extension to the Chrome Web Store if on the master branch
 if [[ 'master' == $TRAVIS_BRANCH ]]
   npm run publish
+else
+  echo "Skipping deploy on branch $TRAVIS_BRANCH"
 fi
