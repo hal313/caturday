9b5a9526a3b94fe957e4fc4bcccb0b7b3ffea3e8
John Ghidiu <jghidiu@solutechnology.com>
2018-04-18 20:49:11 -0400

    Updated build scripts

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,8 +2,9 @@
 
 ## Publishes the extension to the Chrome Web Store if on the master branch
 if [[ 'master' == $TRAVIS_BRANCH ]]; then
-  echo "Publishing on branch $TRAVIS_BRANCH"
+  echo "Publishing on branch: $TRAVIS_BRANCH"
   grunt publish --clientId=$CLIENT_ID --clientSecret=$CLIENT_SECRET --refreshToken=$REFRESH_TOKEN
 else
-  echo "Skipping deploy on branch $TRAVIS_BRANCH"
+  echo "Building on branch: $TRAVIS_BRANCH"
+  grunt build
 fi
