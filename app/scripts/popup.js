5a637b3d98c88f4a90b2b5da5798a9867e5fb8e0
John Ghidiu <john-github@rmdashrf.org>
2021-03-08 14:27:49 -0500

    Fixed resizes

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -10,18 +10,12 @@
      * Populates an image.
      */
     var populateImage = function populateImage() {
-        // The cache bust tag
-        var cacheBust = new Date().getTime();
-  
-        // Remove the existing elements
-        $('#main-image-placeholder').empty();
-  
-        // Add more elements
-        $('#main-image-placeholder').append(
-          '<img id="main-image" src="http://thecatapi.com/api/images/get?format=src&type=gif&cachebust=' +
-          cacheBust +
-          '">'
-        );
+        // The image URL (with a cachebust)
+        var imageUrl = 'https://thecatapi.com/api/images/get?format=src&type=gif&cachebust=' + new Date().getTime();
+        // NOTE: Use this for testing purposes; the dimensions can change in order to test images of different sizes
+        // var imageUrl = 'https://via.placeholder.com/350x150';
+
+        $('#main-image').attr('src', imageUrl);
       };
 
     // The "next" button handler
